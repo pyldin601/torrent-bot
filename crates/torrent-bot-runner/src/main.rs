605153071e30e2001282f98b5c34f3d98c0fc9e5
Roman Lakhtadyr <roman.lakhtadyr@gmail.com>
2025-01-21 20:12:58 +0000

    Set logging to Level::DEBUG

--- crates/torrent-bot-runner/src/main.rs
+++ crates/torrent-bot-runner/src/main.rs
@@ -22,7 +22,7 @@ async fn main() -> std::io::Result<()> {
     };
 
     let subscriber = FmtSubscriber::builder()
-        .with_max_level(Level::INFO)
+        .with_max_level(Level::DEBUG)
         .finish();
 
     tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
